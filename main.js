a4d16c26d1f6220082fca6a5a580195633f6b162
Vintizer <vizerkharkiv@gmail.com>
2019-05-15 23:43:57 +0300

    Auto-generated commit

--- main.js
+++ main.js
@@ -1534,7 +1534,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    configUrl: 'http://localhost:3000/'
+    configUrl: 'https://agile-retreat-51594.herokuapp.com/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
